face2ac60cfefd98c2cf66183a8226cf07bdf741
Darko Striga <darkostriga@Darkos-MacBook-Pro.local>
2020-11-22 00:33:54 +0100

    prod_conf DB

--- config/prod.exs
+++ config/prod.exs
@@ -1,3 +1,10 @@
 use Mix.Config
 
 config :parking_lot, port: String.to_integer(System.get_env("PORT") || "8080")
+
+config :parking_lot, Database.Repo,
+  ssl: true,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "15")
+
+config :parking_lot, ecto_repos: [Database.Repo]
